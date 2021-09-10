a370e5b6563655b92fe206177af464fd70ca3d0f
Willian17 <willianfernandes113@gmail.com>
2021-09-09 21:14:11 -0400

    initial aula01

---
+++ aulas/aula01/gdrive-webapi/src/routes.js
@@ -0,0 +1,40 @@
+import { logger } from "./logger.js"
+
+export default class Routes {
+    io
+    constructor(){
+
+    }
+
+    setSocketInstance(io){
+        this.io = io
+    }
+
+    async defaultRoute(request, response){
+        response.end()
+    }
+
+    async options(request, response){
+        response.writeHead(204)
+        response.end()
+    }
+
+    async post(request, response){
+        logger.info('post')
+        response.end()
+    }
+    async get(request, response){
+        logger.info('get')
+        response.end()
+    }
+
+    async handler(request, response){
+        response.setHeader('Access-Control-Allow-Origin', '*')
+        const chosen = this[request.method.toLowerCase() || this.defaultRoute]
+
+        return chosen.apply(this, [request, response])
+    }
+
+
+
+}
---
+++ aulas/aula01/gdrive-webapi/test/example.test.js
@@ -0,0 +1,11 @@
+import {
+    describe,
+    test,
+    expect
+} from '@jest/globals'
+
+describe('Example', () => {
+    test('should return true', () => {
+        expect(true).toBe(true)
+    })
+})
